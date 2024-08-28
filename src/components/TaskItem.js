d81b9d102239aa69c40347d9d820e0e1dbae1c12
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-08-28 13:24:35 -0300

    feat: add TaskItem component

---
+++ src/components/TaskItem.js
@@ -0,0 +1,8 @@
+const TaskItem = ({task}) =>{
+    return (<>
+    <h1>{task.description}</h1>
+    <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
+    </>);
+};
+
+export default TaskItem;
