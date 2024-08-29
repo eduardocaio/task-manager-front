e351f5e4b4843d78220f5b4d8458438e49c7ac44
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-08-29 18:20:23 -0300

    fix: add return to CustomButton component

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,9 @@
 import "./CustomButton.scss";
 
-const CustomButton = ({onClick, children}) => {
-
-    <div className="custom-button-container" onClick={onClick}>{children}</div>
-
+const CustomButton = ({ onClick, children }) => {
+    return (
+        <div className="custom-button-container" onClick={onClick}>{children}</div>
+    )
 }
+
+export default CustomButton;
