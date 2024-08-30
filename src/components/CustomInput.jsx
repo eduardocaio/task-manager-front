6b5b13d3908d9f6525139c28111629859641f2df
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-08-30 15:20:41 -0300

    feat: add onEnterPress prop

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,18 +1,25 @@
 import "./CustomInput.scss"
 
-const CustomInput = ({label, value, onChange}) =>{
-    return(
+const CustomInput = ({ label, value, onChange, onEnterPress }) => {
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            onEnterPress();
+        }
+    }
+
+    return (
 
         <div className="custom-input-container">
-            <input type="text" className="custom-input" value={value} onChange={(e) => onChange(e)}/>
+            <input type="text" className="custom-input" value={value} onChange={(e) => onChange(e)} onKeyDown={(e) => handleKeyDown(e)} />
 
             {label ? (<label className={`${value.length > 0 ? "shrink" : ""} custom-input-label`}>{label}</label>) : null}
-            
-        
+
+
         </div>
 
     )
-    
+
 
 }
 
