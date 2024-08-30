35a62e62ed66facf373407f80674896013e6d831
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-08-30 15:15:11 -0300

    feat: add function to change a Task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -20,12 +20,26 @@ const TaskItem = ({ task, fetchTasks }) => {
         }
     }
 
+    const handleTaskStatus = async () => {
+        try {
+            await axios.put(`http://localhost:8080/tasks/${task.id}`);
+
+            await fetchTasks();
+
+            alert.success("A tarefa foi modificado com sucesso!");
+        } catch (error) {
+
+            alert.error("Algo deu errado")
+            
+        }
+    }
+
     return (
         <div className="task-item-container">
             <div className="task-description">
                 <label className={task.status ? 'checkbox-container-completed' : 'checkbox-container'}>
                     {task.description}
-                    <input type="checkbox" defaultChecked={task.status}/>
+                    <input type="checkbox" defaultChecked={task.status} onChange={handleTaskStatus}/>
                     <span className={task.status ? 'checkmark completed' : 'checkmark'}></span>
                 </label>
             </div>
