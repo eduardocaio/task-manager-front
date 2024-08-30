4033c0bfe08cccc87e00d0240a1d33de2d67958a
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-08-30 15:20:52 -0300

    feat: add task on enter press

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,7 +8,7 @@ import { FaPlus } from "react-icons/fa6";
 import axios from "axios";
 import { useAlert } from "react-alert";
 
-const AddTask = ({fetchTasks}) => {
+const AddTask = ({ fetchTasks }) => {
     const [task, setTask] = useState("")
 
     const alert = useAlert();
@@ -16,14 +16,14 @@ const AddTask = ({fetchTasks}) => {
     const onChange = (e) => {
         setTask(e.target.value);
     }
-    
+
 
     const handleTaskAddition = async () => {
         try {
-            if (task.length === 0){
+            if (task.length === 0) {
                 return alert.error("A tarefa precisa de uma descrição para ser adicionada!")
             }
-            await axios.post('http://localhost:8080/tasks', {description: task, status: false});
+            await axios.post('http://localhost:8080/tasks', { description: task, status: false });
 
             await fetchTasks();
 
@@ -36,10 +36,10 @@ const AddTask = ({fetchTasks}) => {
         }
     }
 
-    return(
+    return (
         <div className="add-task-container">
-            <CustomInput label="Adicionar tarefa..." value={task} onChange={onChange}/>
-            <CustomButton onClick={handleTaskAddition}><FaPlus size={14} color="#ffffff"/></CustomButton>
+            <CustomInput label="Adicionar tarefa..." value={task} onChange={onChange} onEnterPress={handleTaskAddition} />
+            <CustomButton onClick={handleTaskAddition}><FaPlus size={14} color="#ffffff" /></CustomButton>
         </div>
     )
 
